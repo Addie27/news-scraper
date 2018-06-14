e423a4531aa78f938aef8e95765e3320a18c1b6b
Adeline Johnson <groblewski.adeline@gmail.com>
2018-06-14 13:17:18 -0400

    can save articles to db

---
+++ public/assets/js/script.js
@@ -0,0 +1,68 @@
+
+
+//   // Whenever someone clicks a p tag
+//   $(document).on("click", "p", function() {
+//     // Empty the notes from the note section
+//     $("#notes").empty();
+//     // Save the id from the p tag
+//     var thisId = $(this).attr("data-id");
+
+//     // Now make an ajax call for the Article
+//     $.ajax({
+//       method: "GET",
+//       url: "/articles/" + thisId
+//     })
+//       // With that done, add the note information to the page
+//       .then(function(data) {
+//         console.log(data);
+//         // The title of the article
+//         $("#notes").append("<h2>" + data.title + "</h2>");
+//         // An input to enter a new title
+//         $("#notes").append("<input id='titleinput' name='title' >");
+//         // A textarea to add a new note body
+//         $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
+//         // A button to submit a new note, with the id of the article saved to it
+//         $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
+
+//         // If there's a note in the article
+//         if (data.note) {
+//           // Place the title of the note in the title input
+//           $("#titleinput").val(data.note.title);
+//           // Place the body of the note in the body textarea
+//           $("#bodyinput").val(data.note.body);
+//         }
+//       });
+//   });
+
+// When you click the save button
+$(document).on("click", ".save", function () {
+  // Grab the id associated with the article from the submit button
+
+  var thisId = $(this).siblings().attr("data-id");
+  var thisTitle = $(this).siblings().text().trim();
+  var thisLink = $(this).siblings().attr("href");
+  console.log(thisId);
+  console.log(thisTitle);
+  console.log(thisLink);
+
+
+  // Run a POST request to change the note, using what's entered in the inputs
+  $.ajax({
+    method: "POST",
+    url: "/saved",
+    data: {
+      _id: thisId,
+      title: thisTitle,
+      link: thisLink
+    }
+  })
+    // With that done
+    .then(function (data) {
+      // Log the response
+      console.log(data);
+
+      // // Empty the notes section
+      $(this).empty();
+    });
+
+});
--- server.js
+++ server.js
@@ -27,16 +27,13 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
 
-
+var links = [];
+var titles = [];
 // Routes
 
 app.get("/", function (req, res) {
-  res.render("index"); 
-});
-
-app.get("/scrape", function (req, res) {
   // First, we grab the body of the html with request
-  request("https://www.nytimes.com/", function(error, response, html) {
+  request("https://www.nytimes.com/", function (error, response, html) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
 
     var $ = cheerio.load(html);
@@ -48,37 +45,85 @@ app.get("/scrape", function (req, res) {
       var result = {};
 
       // Add the text and href of every link, and save them as properties of the result object
-      result.title = $(this)
+      var title = result.title = $(this)
         .children("a")
         .text();
-      result.link = $(this)
+      var link = result.link = $(this)
         .children("a")
         .attr("href");
 
-      // Create a new Article using the `result` object built from scraping
-      db.Article.create(result)
-        .then(function (dbArticle) {
-          // View the added result in the console
-          console.log(dbArticle);
-        })
-        .catch(function (err) {
-          // If an error occurred, send it to the client
-          return res.json(err);
-        });
+      if (title && link) {
+        // Insert the data in the scrapedData db
+
+
+        // Create a new Article using the `result` object built from scraping
+        db.Article.create(result)
+          .then(function (dbArticle) {
+            // View the added result in the console
+            // console.log(dbArticle);
+
+          })
+          .catch(function (err) {
+            console.log(err);
+            // If an error occurred, send it to the client
+            //return res.json(err);
+          });
+
+      }
     });
 
     // If we were able to successfully scrape and save an Article, send a message to the client
-    res.send("Scrape Complete");
+    console.log("Scrape Complete");
+
+    res.redirect("/articles");
   });
+
 });
+
+
 
 // Route for getting all Articles from the db
 app.get("/articles", function (req, res) {
-  // TODO: Finish the route so it grabs all of the articles
+
   db.Article.find({})
     .then(function (dbArticle) {
+      // console.log(dbArticle);
+      res.render("index", { dbArticle });
+    })
+    .catch(function (err) {
+      // If an error occurs, send the error back to the client
+      res.json(err);
+    });
+
+});
+
+
+
+// Route for saving an article 
+app.post("/saved", function (req, res) {
+  db.Saved.create(req.body)
+    .then(function (dbSaved) {
+      // View the added result in the console
+      console.log(dbSaved);
+
+    })
+    .catch(function (err) {
+      console.log(err);
+      // If an error occurred, send it to the client
+      //return res.json(err);
+    });
+
+});
+
+
+app.get("/saved", function (req, res) {
+
+  db.Saved.find({})
+
+    .then(function (savedArticle) {
       // If all Notes are successfully found, send them back to the client
-      res.json(dbArticle);
+
+      res.render("saved", savedArticle);
     })
     .catch(function (err) {
       // If an error occurs, send the error back to the client
@@ -86,33 +131,33 @@ app.get("/articles", function (req, res) {
     });
 });
 
-// Route for grabbing a specific Article by id, populate it with it's note
-app.get("/articles/:id", function (req, res) {
+// // Route for grabbing a specific Article by id, populate it with it's note
+// app.post("/articles/id", function (req, res) {
 
-  db.Article.find({ _id: req.params.id })
-    .populate("note")
-    .then(function (dbArticle) {
-      // If all Notes are successfully found, send them back to the client
-      res.json(dbArticle);
-    })
-    .catch(function (err) {
-      // If an error occurs, send the error back to the client
-      res.json(err);
-    });
-});
+//   db.Article.find({ _id: req.params.id })
+//     .populate("note")
+//     .then(function (dbArticle) {
+//       // If all Notes are successfully found, send them back to the client
+//       res.json(dbArticle);
+//     })
+//     .catch(function (err) {
+//       // If an error occurs, send the error back to the client
+//       res.json(err);
+//     });
+// });
 
-// Route for saving/updating an Article's associated Note
-app.post("/articles/:id", function (req, res) {
-  
-  db.Note.create(req.body)
-  .then(function(newNote) {
-    return db.Article.findOneAndUpdate({_id: req.params.id}, {note: newNote._id}, {new : true})
-  })
-  .then(function (data) {
-    res.json(data)
-  })
-  
-});
+// // Route for saving/updating an Article's associated Note
+// app.post("/articles/:id", function (req, res) {
+
+//   db.Note.create(req.body)
+//   .then(function(newNote) {
+//     return db.Article.findOneAndUpdate({_id: req.params.id}, {note: newNote._id}, {new : true})
+//   })
+//   .then(function (data) {
+//     res.json(data)
+//   })
+
+// });
 
 // Start the server
 app.listen(PORT, function () {
