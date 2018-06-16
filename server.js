58fe8eda933171dcbae3691c76c12b684bfcdfad
Adeline Johnson <groblewski.adeline@gmail.com>
2018-06-16 17:37:52 -0400

    trying to figure out heroku

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.set("view engine", "handlebars");
 
 
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-var MONGODB_URI = process.env.MONGOLAB_NAVY_URI|| "mongodb://localhost/mongoHeadlines";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
