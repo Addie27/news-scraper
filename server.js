37113ddfefeef793a10af458ea3c7ee10db16ad4
Adeline Johnson <groblewski.adeline@gmail.com>
2018-06-16 17:31:16 -0400

    updated mongodb

--- server.js
+++ server.js
@@ -20,12 +20,12 @@ app.set("view engine", "handlebars");
 
 
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+var MONGODB_URI = process.env.MONGOLAB_NAVY_URI|| "mongodb://localhost/mongoHeadlines";
 
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
-mongoose.connect(MONGODB_URI);
+mongoose.connect(MONGOLAB_NAVY_URI);
 
 var links = [];
 var titles = [];
